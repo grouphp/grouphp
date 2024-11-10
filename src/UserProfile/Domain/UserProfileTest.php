bf4a2fcda50188d504f7a21ab3e54a7b8ffd5fd5
Patrick Heller <ph@khero.info>
2024-11-10 14:35:13 +0100

    verify that password hashing works

--- src/UserProfile/Domain/UserProfileTest.php
+++ src/UserProfile/Domain/UserProfileTest.php
@@ -4,11 +4,29 @@ namespace App\UserProfile\Domain;
 
 use App\UserProfile\Domain\Event\RegistrationStarted;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\PasswordHasher\Hasher\NativePasswordHasher;
 use Symfony\Component\PasswordHasher\Hasher\PlaintextPasswordHasher;
 
 final class UserProfileTest extends TestCase
 {
-    public function testAccountCreation(): void
+    public function testHashesPassword(): void
+    {
+        $id = UserProfileId::generate();
+        $profile = UserProfile::startWithRegistration(
+            $id,
+            'foo@email.com',
+            'password124',
+            new NativePasswordHasher()
+        );
+
+        self::assertCount(1, $profile->releaseEvents());
+
+        self::assertSame('foo@email.com', $profile->email());
+        // It's not hashed since we use the PlainTextPasswordHasher in that case...
+        self::assertNotSame('password124', $profile->password());
+    }
+
+    public function testRegistration(): void
     {
         $id = UserProfileId::generate();
         $profile = UserProfile::startWithRegistration(
@@ -24,5 +42,9 @@ final class UserProfileTest extends TestCase
             ],
             $profile->releaseEvents()
         );
+
+        self::assertSame('foo@email.com', $profile->email());
+        // It's not hashed since we use the PlainTextPasswordHasher in that case...
+        self::assertSame('password124', $profile->password());
     }
 }
