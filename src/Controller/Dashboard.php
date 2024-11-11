eb9b5cce9ff74de9fe637ddee5d06139cdc584dc
Patrick Heller <ph@khero.info>
2024-11-11 10:49:29 +0100

    adds login & logout functionality

--- src/Controller/Dashboard.php
+++ src/Controller/Dashboard.php
@@ -7,11 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+// TODO: where should I place that one?
 #[Route('dashboard')]
 final class Dashboard extends AbstractController
 {
     public function __invoke(UserInterface $user): Response
     {
-        return new Response('<body>OK</body>');
+        return $this->render('dashboard.html.twig');
     }
 }
