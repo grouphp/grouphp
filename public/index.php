ebd92cb58bf02a4d8030d029cf637f52a5e6542f
Patrick Heller <ph@khero.info>
2024-11-16 15:26:53 +0100

    uses new phpstan level 10

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php declare(strict_types=1);
 
 use App\Kernel;
+use Webmozart\Assert\Assert;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    $env = $context['APP_ENV'];
+    Assert::string($env);
+    return new Kernel($env, (bool) $context['APP_DEBUG']);
 };
